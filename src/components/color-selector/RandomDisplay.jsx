bca1e9b63709fd0365690f74964d8677ac62a6d2
Cristian Montes <cmontesmujica@gmail.com>
2021-11-01 16:00:41 -0600

    added RandomDisplay component

---
+++ src/components/color-selector/RandomDisplay.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const RandomDisplay = ({ showingColor }) => {
+    return(
+        <div
+            style={{ 
+                backgroundColor: showingColor,
+                height: 100,
+                width: 100,
+                marginTop: 20,
+            }}
+        >
+        </div>
+    )
+}
+
+export default RandomDisplay;
--- src/containers/RandomColor.jsx
+++ src/containers/RandomColor.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import RandomDisplay from '../components/color-selector/RandomDisplay.jsx'
 
 class RandomColor extends Component {
  state = {
-     showingColor: 'pink'
- }
+     showingColor: 'red',
+ };
 
+ changeColorState = (colores) => {
+     this.setState({ showingColor: colores })
+ };
 
+render(){
+    const colores = ['red', 'yellow', 'blue', 'green', 'violet', 'teal', 'gold'];
+    const { showingColor } = this.state;
+    return(
+        <>
+            <h1>The random color generator</h1>
+            {/* <SmartColor/> */}
+            <RandomDisplay showingColor={showingColor}/>
+
+        </>
+    )
+}
  
 }
 
