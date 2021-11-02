7d80a7061ec17bb888a26e8df3eddf66fd739031
Cristian Montes <cmontesmujica@gmail.com>
2021-11-01 22:12:11 -0600

    basic styling

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import RandomColor from '../../containers/RandomColor';
-import './main.css';
+import './App.css'
 
 export default function App() {
   return(
-    <RandomColor/>
+    <RandomColor />
   )
 }
